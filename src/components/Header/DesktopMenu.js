878b759eb7749235139ac96a86a55eac8b86ebc5
Alex Raymond <Alexraymond711@gmail.com>
2020-05-20 18:39:14 -0600

    update header with MLS

--- src/components/Header/DesktopMenu.js
+++ src/components/Header/DesktopMenu.js
@@ -21,7 +21,7 @@ const DesktopMenu = ({location: {pathname}, token, cartCount, signout}) => {
           header
         >
           <Logo />
-          Starter Store
+          MLS Artistics
         </Menu.Item>
         {token ? (
           <Menu.Menu position="right">
